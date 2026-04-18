e24dc49a0217bd639e38d0b94d195a8e518918b6
AnudeepKulkarni <anudeep.kulkarni@dataeconomy.io>
2026-04-18 12:19:58 +0530

    Architechture design commit

--- src/main/java/com/hcl/hackaton/config/MongoConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.hackaton.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@Configuration
-@EnableMongoRepositories(basePackages = "com.hcl.hackaton.mongo.repository")
-public class MongoConfig {
-}
--- src/main/java/com/hcl/hackaton/mongo/repository/CustomerMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hcl.hackaton.mongo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.hcl.hackaton.entity.CustomerDocument;
-
-public interface CustomerMongoRepository extends MongoRepository<CustomerDocument, String> {
-}
